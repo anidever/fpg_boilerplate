1251d0f07298fed86c78fd7ff4db2d1c8aedc55e
anidever <anidever@outlook.com>
2021-01-16 18:45:44 -0500

    Add routes

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,9 +1,20 @@
-from flask import jsonify
-from flask_app import app
-from flask_app.models import MockModel
-from flask_app.validators import MockForm
+from flask import jsonify, request
+from http import HTTPStatus
+from flask_app import app, db
+from flask_app.models import MockModel, User
+from flask_app.validators import MockSchema, UserSchema
 
 
 @app.route('/', methods=['GET'])
 def hello_world():
     return jsonify(hello="world")
+
+
+@app.route("/register", methods=['POST'])
+def register():
+    body = request.get_json()
+    data = UserSchema().load(body)
+    user = User(username=data["username"], email=data["email"])
+    db.session.add(user)
+    db.session.commit()
+    return jsonify('GREAT SUCCESS'), HTTPStatus.CREATED
