940e2602a3da1bf808a13f5c73e7473e4c460d75
anidever <anidever@outlook.com>
2021-01-16 18:17:33 -0500

    Add validator

--- flask_app/validators.py
+++ flask_app/validators.py
@@ -1,6 +1,15 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
+from marshmallow import fields, validate, ValidationError
+from flask_app import marshmallow
+from flask_app import models
 
 
-class MockForm(FlaskForm):
+class MockSchema(marshmallow.SQLAlchemySchema):
     pass
+
+
+class UserSchema(marshmallow.SQLAlchemySchema):
+    class Meta:
+        model = models.User
+
+    username = fields.Str(required=True)
+    email = fields.Str(required=True, validate=validate.Email(error="Not a valid email address"))
