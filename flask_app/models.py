d7f542a304ac03299b2017fafe3a14922359596c
anidever <anidever@outlook.com>
2021-01-16 18:45:53 -0500

    Add User table

--- flask_app/models.py
+++ flask_app/models.py
@@ -3,4 +3,8 @@ from flask_app import db
 
 class MockModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
