1f0d6e70adcf36fe0b8b1d500bc4d11f63b9adbf
anidever <anidever@outlook.com>
2021-01-16 13:04:19 -0500

    Add db creating script

---
+++ create_db.py
@@ -0,0 +1,25 @@
+import psycopg2
+from urllib.parse import urlparse
+from app.config import Config
+
+
+if __name__ == "__main__":
+    db_uri = Config.SQLALCHEMY_DATABASE_URI
+    db = urlparse(db_uri)
+    db_name = db.path.lstrip("/")
+
+    con = psycopg2.connect(
+        host=db.hostname,
+        user=db.username,
+        password=db.password,
+        port=db.port,
+        dbname=db_name,
+    )
+    con.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    cursor = con.cursor()
+
+    try:
+        cursor.execute(f"CREATE DATABASE {db_name}")
+        cursor.commit()
+    except Exception:
+        pass
